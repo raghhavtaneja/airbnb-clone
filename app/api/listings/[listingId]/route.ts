2e4dc8dc72d2a1cca978711d29c6e5cea1242fb7
raghav taneja <raghhavtaneja@gmail.com>
2023-07-20 14:08:52 +0530

    updated listing route

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -1,13 +1,16 @@
-import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
-import prisma from "@/app/libs/prismadb";
 
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
 
 interface IParams {
     listingId?: string;
 }
 
-export async function DELETE({ params }: { params: IParams }) {
+export async function DELETE(
+    request: Request,
+    { params }: { params: IParams }
+) {
     const currentUser = await getCurrentUser();
 
     if (!currentUser) {
@@ -17,15 +20,15 @@ export async function DELETE({ params }: { params: IParams }) {
     const { listingId } = params;
 
     if (!listingId || typeof listingId !== 'string') {
-        throw new Error("Invalid Listing ID")
+        throw new Error('Invalid ID');
     }
 
-    const listing = await prisma.reservation.deleteMany({
+    const listing = await prisma.listing.deleteMany({
         where: {
             id: listingId,
             userId: currentUser.id
         }
-    })
+    });
 
     return NextResponse.json(listing);
 }
